eea2d14be36798e42aea9280618d6381c1ad406d
Rudra Narayan Boitei <95564961+Rdrudra99@users.noreply.github.com>
2023-06-17 08:11:58 +0530

    adding meta tag and meta description

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 import "@/styles/globals.css";
 import { UserProvider } from "@/context/authContext";
 import { ChatContextProvider } from "@/context/chatContext";
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
     return (
+      <>
+        <Head>
+          <title>Linkstack || Rudra Narayan Boitei</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description" content="Linkstack is a social media platform where you can share your links with your friends and family." />
+        </Head>
         <UserProvider>
-            <ChatContextProvider>
-                <Component {...pageProps} />
-            </ChatContextProvider>
+          <ChatContextProvider>
+            <Component {...pageProps} />
+          </ChatContextProvider>
         </UserProvider>
+      </>
     );
-}
+  }
+  
